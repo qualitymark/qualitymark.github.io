2105c73e0ae0076e039407e48ed1ad8c985a9625
SuhailG <zonyc17@msn.com>
2019-05-28 04:40:33 -0600

    Add build

---
+++ build/precache-manifest.959eb8286e724cd209d5657fcb474318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9891b0880a5fc7c7b73433b75eef1365",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15a003a3d5e8763ff0c9",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d293cb7bd9995f5cb193",
+    "url": "/static/js/2.7d95a507.chunk.js"
+  },
+  {
+    "revision": "15a003a3d5e8763ff0c9",
+    "url": "/static/js/main.38a7e516.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "97c8cfdcb357a748a132a70ed3c62aee",
+    "url": "/static/media/logo.97c8cfdc.jpg"
+  }
+]);
