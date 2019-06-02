8a820e898c179db6cd3b97606610aac519e6163f
SuhailG <zonyc17@msn.com>
2019-06-02 05:06:50 -0600

    Export addUser which adds a new user to the db if email doesn't exist

--- src/database/dbcon.js
+++ src/database/dbcon.js
@@ -1,3 +1,5 @@
+/*THIS FILE WILL NEED TO BE MOVED TO A BACKEND SERVER AS REACT CLIENTS CANNOT DIRECTLY INTERFACE
+WITH POSTGRESQL */
 const { Client } = require('pg')
 
 const client = new Client({
@@ -10,48 +12,76 @@ const client = new Client({
 })
 client.connect()
 
-const queryDB = async(queryString, values=null)=>{
-    try{
-        let res = await client.query(queryString)
-        return(res.rows)
-    }catch(e){console.error(e)}
+const queryDB = async (queryString, values = null) => {
+    try {
+        const res = await client.query(queryString)
+        return (res.rows)
+    } catch (e) { console.error(e) }
 }
 
 const getUsers = async () => {
-    let queryString = 'SELECT * FROM public.users;'
-    let users = await queryDB(queryString)
+    const queryString = 'SELECT * FROM public.users;'
+    const users = await queryDB(queryString)
     return users
     // console.log(users)
 }
 
 const emailExists = async (email) => {
-    let queryString = `SELECT * FROM public.users WHERE email_address = '${email}';`
-    let user = await queryDB(queryString)
+    const queryString = `SELECT * FROM public.users WHERE email_address = '${email}';`
+    const user = await queryDB(queryString)
+    console.log(user.length ? true : false)
     return user.length ? true : false
 }
 
-const addUser = async({firstName=null, lastName=null, email, username, password}) =>{
-    let queryString = `INSERT INTO public.users VALUES`
+const addUser = async (firstName = null, lastName = null, email, username, password) => {
+    if (!await emailExists(email)) {
+        const queryString = `INSERT INTO public.users 
+    (
+        first_name, 
+        last_name,
+        email_address, 
+        username,
+        password, 
+        last_update
+    ) 
+    VALUES (
+        '${firstName}',
+        '${lastName}',
+        '${email}',
+        '${username}',
+        '${password}', 
+        '${new Date().toLocaleString()}')
+    RETURNING id`
+        let user = await (queryDB(queryString))
+        console.log(user)
+        return(user)
+    }
+    else
+        throw new Error('Email already registered')
 }
 
 const validateUsers = async (emailaddr, pwd) => {
-	// Added by Daniel M: Returns 0 if invalid uid / pwd and a 1 if it is valid
-	// based on front end, it looks like it is using the email as the user id.
+    // Added by Daniel M: Returns 0 if invalid uid / pwd and a 1 if it is valid
+    // based on front end, it looks like it is using the email as the user id.
     let queryString = `select count(*) from public.users where email_address = '${emailaddr}' and password='${pwd}';`
     let usercount = await queryDB(queryString)
     return usercount
     // console.log(users)
 }
 
-const closeConnection = async()=>{
-    try{
+const closeConnection = async () => {
+    try {
         await client.end()
-    }catch(e){console.error(e)}
+    } catch (e) { console.error(e) }
 }
-
-(async()=>{
-    closeConnection()
-})()
+module.exports = {addUser:addUser}
+// (async () => {
+//     // let x = await emailExists('js@me.com')
+//     // console.log(x)
+//     // try{let user= await addUser('John', 'Smithson', 'json@me.com', 'json1994', 'object')}catch(e){console.error(e)}
+//     // console.log(user)
+//     closeConnection()
+// })()
 
 
 
